71c9601d69a05f29174d5258342b67b893a054e8
peter.w.vanhemert.ctr <peter.w.vanhemert.ctr@mail.mil>
2026-03-17 15:40:22 -0400

    Created and signed pbo for custom vcomai mod

---
+++ VcomTest.Stratis/vcom_ai_v4.3.0-tdsa/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 721359761;
+name = "Vcom AI V3.4.0";
+timestamp = 5249018273123046173;
---
+++ VcomTest.Stratis/vcom_ai_v4.3.0-tdsa/mod.cpp
@@ -0,0 +1,23 @@
+name = "VCOMAI";
+picture = "VcomAILLogo.paa";
+actionName = "Website";
+action = "https://forums.bohemia.net/forums/topic/216611-release-vcom-ai-3-dangerous-ai/";
+description = "Enhancing your ArmA 3 experience to the next level.";
+logo = "VcomAILogo.paa";
+logoOver = "VcomAILLogo.paa";
+tooltip = "Vcom";
+tooltipOwned = "Genesis92x";
+overview = "Bringing the AI back into the fight.";
+author = "Genesis92x";
+overviewPicture = "";
+overviewText = "VcomAI";
+overviewFootnote = "Vcom is under ARMA PUBLIC LICENSE SHARE ALIKE (APL-SA)";
+hideName = 0;
+hidePicture = 0;
+dlcColor[] = 
+{
+	0.23,
+	0.39,
+	0.30,
+	1
+};
